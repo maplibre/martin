83ec1b41dbb2d33189b063590a3bf17078f8c482
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-10-11 14:26:12 +0300

    fix path in processingData

--- processingData.sh
+++ processingData.sh
@@ -2,14 +2,15 @@
 
 for i in 01 02 03 04 05 06 07 08 09 10 11 12
 do
-    fileName=/data/yellow_tripdata_2017-${i}.csv
+    fileName=yellow_tripdata_2017-${i}.csv
+    filePath=/data/${fileName}
 
-    if [[ ! -e ${fileName} ]]
+    if [[ ! -e ${filePath} ]]
     then
-        wget -O ${fileName} "https://s3.amazonaws.com/nyc-tlc/trip+data/${fileName}"
-        dos2unix ${fileName}
-        sed -i -e '2d' ${fileName}
+        wget -O ${filePath} "https://s3.amazonaws.com/nyc-tlc/trip+data/${fileName}"
+        dos2unix ${filePath}
+        sed -i -e '2d' ${filePath}
         dockerize -wait tcp://db:5432 -timeout 1m
-        PGPASSWORD=postgres psql -U postgres -d db -h db -c "COPY trips FROM '${fileName}' WITH csv header;"
+        PGPASSWORD=postgres psql -U postgres -d db -h db -c "COPY trips FROM '${filePath}' WITH csv header;"
     fi
 done
