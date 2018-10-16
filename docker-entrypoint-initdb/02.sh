b5d554faca34a337a52769a2ea7775af947174eb
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-10-16 14:04:22 +0300

    process taxi_zones & add to db

--- docker-entrypoint-initdb/02.sh
+++ docker-entrypoint-initdb/02.sh
@@ -2,6 +2,7 @@
 
 wget -O ${HOME}/taxi_zones.zip https://s3.amazonaws.com/nyc-tlc/misc/taxi_zones.zip
 unzip ${HOME}/taxi_zones.zip -d ${HOME}
+ogr2ogr -f PostgreSQL PG:dbname=db -nln taxi_zones ${HOME}/taxi_zones.shp
 
 for i in 01 02 03 04 05 06 07 08 09 10 11 12
 do
