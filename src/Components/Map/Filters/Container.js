1c548efc7e6253ea5efa216a9b2fd4602fa56511
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-10-22 13:16:22 +0300

    fix filters width

--- src/Components/Map/Filters/Container.js
+++ src/Components/Map/Filters/Container.js
@@ -4,10 +4,12 @@ export default styled.div`
   position: absolute;
   top: 0;
 
-  max-width: 450px;
+  width: 350px;
   padding: 25px;
   border-bottom-right-radius: 5px;
   box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.5);
   background-color: rgba(28,28,48,0.9);
   color: #dadfee;
+
+  z-index: 2;
 `;
