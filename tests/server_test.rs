639ad1c52efabb767e704289182ad9e0dbd3eb77
Stepan Kuzmin <stepankuzmin@yandex-team.ru>
2020-04-26 16:22:24 +0300

    test: upgrade tests to actix-web 2.0

--- tests/server_test.rs
+++ tests/server_test.rs
@@ -1,6 +1,5 @@
 extern crate log;
 
-use actix_web::dev::Service;
 use actix_web::{http, test, App};
 
 use martin::dev::{mock_function_sources, mock_state, mock_table_sources};
@@ -8,100 +7,96 @@ use martin::function_source::FunctionSources;
 use martin::server::router;
 use martin::table_source::TableSources;
 
-#[test]
-fn test_get_table_sources_ok() {
-  let state = test::run_on(|| mock_state(mock_table_sources(), None));
-  let mut app = test::init_service(App::new().data(state).configure(router));
+#[actix_rt::test]
+async fn test_get_table_sources_ok() {
+  let state = mock_state(mock_table_sources(), None);
+  let mut app = test::init_service(App::new().data(state).configure(router)).await;
 
   let req = test::TestRequest::get().uri("/index.json").to_request();
-
-  let response = test::block_on(app.call(req)).unwrap();
+  let response = test::call_service(&mut app, req).await;
   assert!(response.status().is_success());
 
-  let body = test::read_body(response);
+  let body = test::read_body(response).await;
   let table_sources: TableSources = serde_json::from_slice(&body).unwrap();
   assert!(table_sources.contains_key("public.table_source"));
 }
 
-#[test]
-fn test_get_table_source_ok() {
-  let state = test::run_on(|| mock_state(mock_table_sources(), None));
-  let mut app = test::init_service(App::new().data(state).configure(router));
+#[actix_rt::test]
+async fn test_get_table_source_ok() {
+  let state = mock_state(mock_table_sources(), None);
+  let mut app = test::init_service(App::new().data(state).configure(router)).await;
 
   let req = test::TestRequest::get()
     .uri("/public.non_existant.json")
     .to_request();
 
-  let response = test::block_on(app.call(req)).unwrap();
+  let response = test::call_service(&mut app, req).await;
   assert_eq!(response.status(), http::StatusCode::NOT_FOUND);
 
   let req = test::TestRequest::get()
     .uri("/public.table_source.json")
     .to_request();
 
-  let response = test::block_on(app.call(req)).unwrap();
+  let response = test::call_service(&mut app, req).await;
   assert!(response.status().is_success());
 }
 
-#[test]
-fn test_get_table_source_tile_ok() {
-  let state = test::run_on(|| mock_state(mock_table_sources(), None));
-  let mut app = test::init_service(App::new().data(state).configure(router));
+#[actix_rt::test]
+async fn test_get_table_source_tile_ok() {
+  let state = mock_state(mock_table_sources(), None);
+  let mut app = test::init_service(App::new().data(state).configure(router)).await;
 
   let req = test::TestRequest::get()
     .uri("/public.table_source/0/0/0.pbf")
     .to_request();
 
-  let future = test::run_on(|| app.call(req));
-  let response = test::block_on(future).unwrap();
+  let response = test::call_service(&mut app, req).await;
   assert!(response.status().is_success());
 }
 
-#[test]
-fn test_get_function_sources_ok() {
-  let state = test::run_on(|| mock_state(None, mock_function_sources()));
-  let mut app = test::init_service(App::new().data(state).configure(router));
+#[actix_rt::test]
+async fn test_get_function_sources_ok() {
+  let state = mock_state(None, mock_function_sources());
+  let mut app = test::init_service(App::new().data(state).configure(router)).await;
 
   let req = test::TestRequest::get().uri("/rpc/index.json").to_request();
-
-  let response = test::block_on(app.call(req)).unwrap();
+  let response = test::call_service(&mut app, req).await;
   assert!(response.status().is_success());
 
-  let body = test::read_body(response);
+  let body = test::read_body(response).await;
   let function_sources: FunctionSources = serde_json::from_slice(&body).unwrap();
   assert!(function_sources.contains_key("public.function_source"));
 }
 
-#[test]
-fn test_get_function_source_ok() {
-  let state = test::run_on(|| mock_state(None, mock_function_sources()));
-  let mut app = test::init_service(App::new().data(state).configure(router));
+#[actix_rt::test]
+async fn test_get_function_source_ok() {
+  let state = mock_state(None, mock_function_sources());
+  let mut app = test::init_service(App::new().data(state).configure(router)).await;
 
   let req = test::TestRequest::get()
     .uri("/rpc/public.non_existant.json")
     .to_request();
 
-  let response = test::block_on(app.call(req)).unwrap();
+  let response = test::call_service(&mut app, req).await;
   assert_eq!(response.status(), http::StatusCode::NOT_FOUND);
 
   let req = test::TestRequest::get()
     .uri("/rpc/public.function_source.json")
     .to_request();
 
-  let response = test::block_on(app.call(req)).unwrap();
+  let response = test::call_service(&mut app, req).await;
   assert!(response.status().is_success());
 }
 
-#[test]
-fn test_get_function_source_tile_ok() {
-  let state = test::run_on(|| mock_state(None, mock_function_sources()));
-  let mut app = test::init_service(App::new().data(state).configure(router));
+#[actix_rt::test]
+async fn test_get_function_source_tile_ok() {
+  let state = mock_state(None, mock_function_sources());
+  let mut app = test::init_service(App::new().data(state).configure(router)).await;
 
   let req = test::TestRequest::get()
     .uri("/rpc/public.function_source/0/0/0.pbf")
     .to_request();
 
-  let future = test::run_on(|| app.call(req));
-  let response = test::block_on(future).unwrap();
+  let response = test::call_service(&mut app, req).await;
   assert!(response.status().is_success());
 }
