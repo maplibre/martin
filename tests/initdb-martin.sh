7d5dfa8174e7348b3b4b7279e699f24e6e129501
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2020-12-27 16:35:05 +0300

    chore: drop mdillon postgis in favor of official image (#157)

---
+++ tests/initdb-martin.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+# Perform all actions as $POSTGRES_USER
+export PGUSER="$POSTGRES_USER"
+
+echo "Loading Martin fixtures into $POSTGRES_DB"
+
+psql --dbname="$POSTGRES_DB" -f /fixtures/TileBBox.sql
+psql --dbname="$POSTGRES_DB" -f /fixtures/points_source.sql
+psql --dbname="$POSTGRES_DB" -f /fixtures/table_source.sql
+psql --dbname="$POSTGRES_DB" -f /fixtures/function_source.sql
