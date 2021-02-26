356cc22a5524127fdbed894e7b59550be61fe3ad
Miguel de Dios Matias <tres.14159@gmail.com>
2021-02-26 12:24:51 +0100

    Fixed another warnings of clippy (from pre-push hook).

--- src/server.rs
+++ src/server.rs
@@ -332,7 +332,8 @@ fn create_state(
 async fn bearer_auth_validator(
     req: dev::ServiceRequest,
     credentials: BearerAuth,
-    ) -> Result<dev::ServiceRequest, Error> {
+    )
+    -> Result<dev::ServiceRequest, Error> {
     let secret = "aaaa";
     
     let try_catch_block = || -> Result<(Verifier, Algorithm), jwt::error::Error> {
