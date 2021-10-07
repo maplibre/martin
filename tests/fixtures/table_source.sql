1a2512992565021c18909afb4f3f807ca5782f03
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2021-10-07 19:54:25 +0300

    chore: improve debug.html (#257)

--- tests/fixtures/table_source.sql
+++ tests/fixtures/table_source.sql
@@ -2,3 +2,5 @@ DROP TABLE IF EXISTS table_source;
 CREATE TABLE table_source(gid serial PRIMARY KEY, geom geometry(GEOMETRY, 4326));
 INSERT INTO table_source(geom) values (GeomFromEWKT('SRID=4326;POINT(0 0)'));
 INSERT INTO table_source(geom) values (GeomFromEWKT('SRID=4326;POINT(-2 2)'));
+INSERT INTO table_source(geom) values (GeomFromEWKT('SRID=4326;LINESTRING(0 0, 1 1)'));
+INSERT INTO table_source(geom) values (GeomFromEWKT('SRID=4326;LINESTRING(2 2, 3 3)'));
