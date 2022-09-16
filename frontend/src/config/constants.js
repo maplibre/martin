baabe0462962ae2e4141a08a4f3dab0bb32cdfaf
Birk Skyum <birk.skyum@pm.me>
2022-09-17 01:23:38 +0200

    https

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,3 +1,3 @@
-export const MAP_STYLE = 'http://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json';
+export const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json';
 export const JAN = new Date(2017, 0);
 export const DEC = new Date(2017, 11);
