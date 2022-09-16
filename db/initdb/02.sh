55345a160172c34f604e0fa48733c44ab5ba8c12
Birk Skyum <birk.skyum@pm.me>
2022-09-16 15:28:32 +0200

    initially only load 1 month of data

--- db/initdb/02.sh
+++ db/initdb/02.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O ${HOME}/taxi_zones.zip https://s3.amazonaws.com/nyc-tlc/misc/taxi_zones.zip
+wget -O ${HOME}/taxi_zones.zip https://martin-documentation-site.s3.us-west-2.amazonaws.com/taxi_zones/taxi_zones.zip
 unzip ${HOME}/taxi_zones.zip -d ${HOME}
 
 geomColumn=geom
@@ -8,13 +8,15 @@ ogr2ogr -f PostgreSQL PG:dbname=db -nln taxi_zones -nlt MULTIPOLYGON -lco GEOMET
 psql -U postgres -d db -c "alter table taxi_zones alter column ${geomColumn} type geometry(multipolygon, 3857) using st_transform(${geomColumn}, 3857); \
   create index idx_taxi_zones_geom on taxi_zones using gist(${geomColumn});"
 
-for i in 01 02 03 04 05 06 07 08 09 10 11 12
+for i in 01 # 02 03 04 05 06 07 08 09 10 11 12
 do
     fileName=yellow_tripdata_2017-${i}.csv
     filePath=${HOME}/${fileName}
 
-    wget -O ${filePath} "https://s3.amazonaws.com/nyc-tlc/trip+data/${fileName}"
+    wget -O ${filePath} "https://martin-documentation-site.s3.us-west-2.amazonaws.com/taxi_trips/taxo/${fileName}"
     dos2unix ${filePath}
+    echo "format data"
     sed -i -e '2d' ${filePath}
+    echo "load into database"
     psql -U postgres -d db -c "COPY trips FROM '${filePath}' WITH csv header;"
 done
