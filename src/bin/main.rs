d72fa060206eddbced4497753a41687659ad4c43
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2022-09-15 22:43:47 -0400

    wip

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,7 @@
 use std::{env, io};
 
 use actix_web::dev::Server;
-use docopt::Docopt;
+use clap::Parser;
 use log::{error, info, warn};
 use martin::config::{read_config, Config, ConfigBuilder};
 use martin::db::{check_postgis_version, get_connection, setup_connection_pool, Pool};
@@ -13,67 +13,88 @@ use serde::Deserialize;
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const REQUIRED_POSTGIS_VERSION: &str = ">= 2.4.0";
 
-pub const USAGE: &str = "
-Martin - PostGIS Mapbox Vector Tiles server.
-
-Usage:
-  martin [options] [<connection>]
-  martin -h | --help
-  martin -v | --version
-
-Options:
-  -h --help                         Show this screen.
-  -v --version                      Show version.
-  --config=<path>                   Path to config file.
-  --keep-alive=<n>                  Connection keep alive timeout [default: 75].
-  --listen-addresses=<n>            The socket address to bind [default: 0.0.0.0:3000].
-  --default-srid=<n>                If a spatial table has SRID 0, then this default SRID will be used as a fallback.
-  --pool-size=<n>                   Maximum connections pool size [default: 20].
-  --workers=<n>                     Number of web server workers.
-  --ca-root-file=<path>             Loads trusted root certificates from a file. The file should contain a sequence of PEM-formatted CA certificates.
-  --danger-accept-invalid-certs     Trust invalid certificates. This introduces significant vulnerabilities, and should only be used as a last resort.
-  --watch                           [IGNORED] This flag is no longer supported, and will be ignored.
-";
-
-#[derive(Debug, Deserialize)]
+#[derive(Parser, Debug, Deserialize)]
+#[clap(about, version)]
 pub struct Args {
-    pub arg_connection: Option<String>,
-    pub flag_config: Option<String>,
-    pub flag_help: bool,
-    pub flag_keep_alive: Option<usize>,
-    pub flag_listen_addresses: Option<String>,
-    pub flag_pool_size: Option<u32>,
-    pub flag_watch: bool,
-    pub flag_version: bool,
-    pub flag_workers: Option<usize>,
-    pub flag_default_srid: Option<i32>,
-    pub flag_ca_root_file: Option<String>,
-    pub flag_danger_accept_invalid_certs: bool,
+    /// Path to config file.
+    #[clap(short, long)]
+    pub config: Option<String>,
+    /// Loads trusted root certificates from a file. The file should contain a sequence of PEM-formatted CA certificates.
+    #[clap(long)]
+    pub ca_root_file: Option<String>,
+    /// Trust invalid certificates. This introduces significant vulnerabilities, and should only be used as a last resort.
+    #[clap(long)]
+    pub danger_accept_invalid_certs: bool,
+    /// If a spatial table has SRID 0, then this default SRID will be used as a fallback.
+    #[clap(short, long)]
+    pub default_srid: Option<i32>,
+    // Must match ConfigBuilder::KEEP_ALIVE_DEFAULT
+    /// Connection keep alive timeout. [DEFAULT: 75]
+    #[clap(short, long)]
+    pub keep_alive: Option<usize>,
+    // Must match ConfigBuilder::LISTEN_ADDRESSES_DEFAULT
+    /// The socket address to bind. [DEFAULT: 0.0.0.0:3000]
+    #[clap(short, long)]
+    pub listen_addresses: Option<String>,
+    // Must match ConfigBuilder::POOL_SIZE_DEFAULT
+    /// Maximum connections pool size [DEFAULT: 20]
+    #[clap(short, long)]
+    pub pool_size: Option<u32>,
+    /// Scan for new sources on sources list requests
+    #[clap(short, long, hide = true)]
+    pub watch: bool,
+    /// Number of web server workers
+    #[clap(short = 'W', long)]
+    pub workers: Option<usize>,
+    /// Database connection string
+    pub connection: Option<String>,
+}
+
+impl Args {
+    fn to_config(self) -> ConfigBuilder {
+        ConfigBuilder {
+            ca_root_file: self.ca_root_file,
+            danger_accept_invalid_certs: if self.danger_accept_invalid_certs {
+                Some(true)
+            } else {
+                None
+            },
+            default_srid: self.default_srid,
+            keep_alive: self.keep_alive,
+            listen_addresses: self.listen_addresses,
+            pool_size: self.pool_size,
+            worker_processes: self.workers,
+            connection_string: self.connection,
+            table_sources: None,
+            function_sources: None,
+        }
+    }
 }
 
 pub async fn generate_config(args: Args, pool: &Pool) -> io::Result<Config> {
-    let connection_string = args.arg_connection.clone().ok_or_else(|| {
-        io::Error::new(
-            io::ErrorKind::Other,
-            "Database connection string is not set",
-        )
-    })?;
+    // let connection_string = args.arg_connection.clone().ok_or_else(|| {
+    //     io::Error::new(
+    //         io::ErrorKind::Other,
+    //         "Database connection string is not set",
+    //     )
+    // })?;
+    let connection_string = args.connection.clone();
 
     let mut connection = get_connection(pool).await?;
-    let table_sources = get_table_sources(&mut connection, &args.flag_default_srid).await?;
+    let table_sources = get_table_sources(&mut connection, &args.default_srid).await?;
     let function_sources = get_function_sources(&mut connection).await?;
 
     let config = ConfigBuilder {
+        ca_root_file: None,
+        danger_accept_invalid_certs: if args.danger_accept_invalid_certs { Some(true) } else { None },
+        default_srid: args.default_srid,
+        keep_alive: args.keep_alive,
+        listen_addresses: args.listen_addresses,
+        pool_size: args.pool_size,
+        worker_processes: args.workers,
         connection_string,
-        keep_alive: args.flag_keep_alive,
-        listen_addresses: args.flag_listen_addresses,
-        default_srid: args.flag_default_srid,
-        pool_size: args.flag_pool_size,
-        worker_processes: args.flag_workers,
         table_sources: Some(table_sources),
         function_sources: Some(function_sources),
-        ca_root_file: None,
-        danger_accept_invalid_certs: Some(args.flag_danger_accept_invalid_certs),
     };
 
     let config = config.finalize();
@@ -89,8 +110,8 @@ async fn setup_from_config(file_name: String) -> io::Result<(Config, Pool)> {
         Some(config.pool_size),
         config.danger_accept_invalid_certs,
     )
-    .await
-    .map_err(|e| prettify_error!(e, "Can't setup connection pool"))?;
+        .await
+        .map_err(|e| prettify_error!(e, "Can't setup connection pool"))?;
 
     if let Some(table_sources) = &config.table_sources {
         for table_source in table_sources.values() {
@@ -119,7 +140,7 @@ async fn setup_from_config(file_name: String) -> io::Result<(Config, Pool)> {
 }
 
 async fn setup_from_args(args: Args) -> io::Result<(Config, Pool)> {
-    let connection_string = args.arg_connection.clone().ok_or_else(|| {
+    let connection_string = args.connection.clone().ok_or_else(|| {
         io::Error::new(
             io::ErrorKind::Other,
             "Database connection string is not set",
@@ -129,12 +150,12 @@ async fn setup_from_args(args: Args) -> io::Result<(Config, Pool)> {
     info!("Connecting to database");
     let pool = setup_connection_pool(
         &connection_string,
-        &args.flag_ca_root_file,
-        args.flag_pool_size,
-        args.flag_danger_accept_invalid_certs,
+        &args.ca_root_file,
+        args.pool_size,
+        args.danger_accept_invalid_certs,
     )
-    .await
-    .map_err(|e| prettify_error!(e, "Can't setup connection pool"))?;
+        .await
+        .map_err(|e| prettify_error!(e, "Can't setup connection pool"))?;
 
     info!("Scanning database");
     let config = generate_config(args, &pool)
@@ -145,11 +166,11 @@ async fn setup_from_args(args: Args) -> io::Result<(Config, Pool)> {
 }
 
 fn parse_env(args: Args) -> Args {
-    let arg_connection = args.arg_connection.or_else(|| {
+    let connection = args.connection.or_else(|| {
         env::var_os("DATABASE_URL").and_then(|connection| connection.into_string().ok())
     });
 
-    let flag_default_srid = args.flag_default_srid.or_else(|| {
+    let default_srid = args.default_srid.or_else(|| {
         env::var_os("DEFAULT_SRID").and_then(|srid| {
             srid.into_string()
                 .ok()
@@ -157,14 +178,14 @@ fn parse_env(args: Args) -> Args {
         })
     });
 
-    let flag_ca_root_file = args.flag_ca_root_file.or_else(|| {
+    let ca_root_file = args.ca_root_file.or_else(|| {
         env::var_os("CA_ROOT_FILE").and_then(|connection| connection.into_string().ok())
     });
 
-    let flag_danger_accept_invalid_certs = args.flag_danger_accept_invalid_certs
-        || env::var_os("DANGER_ACCEPT_INVALID_CERTS").is_some();
+    let danger_accept_invalid_certs =
+        args.danger_accept_invalid_certs || env::var_os("DANGER_ACCEPT_INVALID_CERTS").is_some();
 
-    if args.flag_watch {
+    if args.watch {
         warn!("The --watch flag is no longer supported, and will be ignored");
     }
     if env::var_os("WATCH_MODE").is_some() {
@@ -172,10 +193,10 @@ fn parse_env(args: Args) -> Args {
     }
 
     Args {
-        arg_connection,
-        flag_default_srid,
-        flag_ca_root_file,
-        flag_danger_accept_invalid_certs,
+        connection,
+        default_srid,
+        ca_root_file,
+        danger_accept_invalid_certs,
         ..args
     }
 }
@@ -183,7 +204,7 @@ fn parse_env(args: Args) -> Args {
 async fn start(args: Args) -> io::Result<Server> {
     info!("Starting martin v{VERSION}");
 
-    let (config, pool) = match args.flag_config {
+    let (config, pool) = match args.config {
         Some(config_file_name) => {
             info!("Using {config_file_name}");
             setup_from_config(config_file_name).await?
@@ -209,36 +230,83 @@ async fn start(args: Args) -> io::Result<Server> {
     Ok(server)
 }
 
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     let env = env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "martin=info");
     env_logger::Builder::from_env(env).init();
 
-    let args = Docopt::new(USAGE)
-        .and_then(|d| d.help(false).deserialize::<Args>())
-        .map_err(|e| prettify_error!(e, "Can't parse CLI arguments"))?;
+    let args: Args = Args::parse();
+    println!("{:?}", &args);
+    let parse: ConfigBuilder = args.to_config();
+    print_type_of(&parse);
+    println!("{:?}", &parse);
+    // parse.map(|args| {
+    //     let args = parse_env(args);
+    //     let runner = start(args)?;
+    //     runner.run();
+    // });
+    // let clap_matches = Args::command().ignore_errors(true).get_matches();
+    // print_type_of(&clap_matches); // clap::parse::matches::arg_matches::ArgMatches
 
-    let args = parse_env(args);
+    // let mut v = Viperus::new();
+    // v.load_clap(clap_matches).unwrap();
+    // let config = clap_matches.value_of("config");
+    // if config {
+    //     v.load_file(config, Format::YAML).unwrap();
+    // }
+    // debug!("final {:?}", v);
+    //
+    // // let x = args.value_of("foo");
+    // // let args = Docopt::new(USAGE)
+    // //     .and_then(|d| d.help(false).deserialize::<Args>())
+    // //     .map_err(prettify_error("Can't parse CLI arguments".to_owned()))?;
+    //
+    // // let args = parse_env(args);
+    // // let args = Args::parse();
+    // let args = Args {
+    //     config: v.get("config").unwrap(),
+    //     keep_alive: v.get("keep_alive").unwrap(),
+    //     listen_addresses: v.get("listen_addresses").unwrap(),
+    //     default_srid: v.get("default_srid").unwrap(),
+    //     pool_size: v.get("pool_size").unwrap(),
+    //     workers: v.get("workers").unwrap(),
+    //     ca_root_file: v.get("ca_root_file").unwrap(),
+    //     danger_accept_invalid_certs: v.get("danger_accept_invalid_certs").unwrap(),
+    //     connection: v.get("connection").unwrap(),
+    // };
+    //
+    //
+    // if args.danger_accept_invalid_certs {
+    //     warn!("Danger accept invalid certs enabled. You should think very carefully before using this option. If invalid certificates are trusted, any certificate for any site will be trusted for use. This includes expired certificates. This introduces significant vulnerabilities, and should only be used as a last resort.");
+    // }
+    //
+    // let server = match start(args) {
+    //     Ok(server) => server,
+    //     Err(error) => {
+    //         error!("{error}");
+    //         std::process::exit(-1);
+    //     }
+    // };
+    //
+    // server.run()
+    // let server = match start(args) {
+    //     Ok(server) => server,
+    //     Err(error) => {
+    //         error!("{error}");
+    //         std::process::exit(-1);
+    //     }
+    // };
 
-    if args.flag_help {
-        println!("{USAGE}");
-        std::process::exit(0);
-    }
-
-    if args.flag_version {
-        println!("v{VERSION}");
-        std::process::exit(0);
-    }
-
-    if args.flag_danger_accept_invalid_certs {
-        warn!("Danger accept invalid certs enabled. You should think very carefully before using this option. If invalid certificates are trusted, any certificate for any site will be trusted for use. This includes expired certificates. This introduces significant vulnerabilities, and should only be used as a last resort.");
-    }
-
-    match start(args).await {
-        Ok(server) => server.await,
-        Err(error) => {
-            error!("{error}");
-            std::process::exit(-1);
-        }
-    }
+    // match start(args).await {
+    //     Ok(server) => server.await,
+    //     Err(error) => {
+    //         error!("{error}");
+    //         std::process::exit(-1);
+    //     }
+    // }
+    Ok(())
 }
--- src/config.rs
+++ src/config.rs
@@ -10,47 +10,72 @@ use crate::utils::prettify_error;
 
 #[derive(Clone, Debug, Serialize)]
 pub struct Config {
-    pub pool_size: u32,
-    pub keep_alive: usize,
-    pub default_srid: Option<i32>,
-    pub worker_processes: usize,
-    pub listen_addresses: String,
-    pub connection_string: String,
-    pub table_sources: Option<TableSources>,
-    pub function_sources: Option<FunctionSources>,
     pub ca_root_file: Option<String>,
     pub danger_accept_invalid_certs: bool,
-}
-
-#[derive(Deserialize)]
-pub struct ConfigBuilder {
-    pub pool_size: Option<u32>,
-    pub keep_alive: Option<usize>,
     pub default_srid: Option<i32>,
-    pub worker_processes: Option<usize>,
-    pub listen_addresses: Option<String>,
+    pub keep_alive: usize,
+    pub listen_addresses: String,
+    pub pool_size: u32,
+    pub worker_processes: usize,
     pub connection_string: String,
     pub table_sources: Option<TableSources>,
     pub function_sources: Option<FunctionSources>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ConfigBuilder {
     pub ca_root_file: Option<String>,
     pub danger_accept_invalid_certs: Option<bool>,
+    pub default_srid: Option<i32>,
+    pub keep_alive: Option<usize>,
+    pub listen_addresses: Option<String>,
+    pub pool_size: Option<u32>,
+    pub worker_processes: Option<usize>,
+    pub connection_string: Option<String>,
+    pub table_sources: Option<TableSources>,
+    pub function_sources: Option<FunctionSources>,
+}
+
+/// Update empty option in place with a non-empty value from the second option.
+fn set_option<T>(first: &mut Option<T>, second: Option<T>) {
+    if first.is_none() && second.is_some() {
+        *first = second;
+    }
 }
 
 impl ConfigBuilder {
+    pub const KEEP_ALIVE_DEFAULT: usize = 75;
+    pub const LISTEN_ADDRESSES_DEFAULT: &'static str = "0.0.0.0:3000";
+    pub const POOL_SIZE_DEFAULT: u32 = 20;
+
+    pub fn merge(&mut self, other: ConfigBuilder) -> &mut Self {
+        set_option(&mut self.ca_root_file, other.ca_root_file);
+        set_option(&mut self.danger_accept_invalid_certs, other.danger_accept_invalid_certs);
+        set_option(&mut self.default_srid, other.default_srid);
+        set_option(&mut self.keep_alive, other.keep_alive);
+        set_option(&mut self.listen_addresses, other.listen_addresses);
+        set_option(&mut self.pool_size, other.pool_size);
+        set_option(&mut self.worker_processes, other.worker_processes);
+        set_option(&mut self.connection_string, other.connection_string);
+        set_option(&mut self.table_sources, other.table_sources);
+        set_option(&mut self.function_sources, other.function_sources);
+        self
+    }
+
     pub fn finalize(self) -> Config {
         Config {
-            pool_size: self.pool_size.unwrap_or(20),
-            keep_alive: self.keep_alive.unwrap_or(75),
-            default_srid: self.default_srid,
-            worker_processes: self.worker_processes.unwrap_or_else(num_cpus::get),
-            listen_addresses: self
-                .listen_addresses
-                .unwrap_or_else(|| "0.0.0.0:3000".to_owned()),
-            connection_string: self.connection_string,
-            table_sources: self.table_sources,
-            function_sources: self.function_sources,
             ca_root_file: self.ca_root_file,
             danger_accept_invalid_certs: self.danger_accept_invalid_certs.unwrap_or(false),
+            default_srid: self.default_srid,
+            keep_alive: self.keep_alive.unwrap_or(Self::KEEP_ALIVE_DEFAULT),
+            listen_addresses: self
+                .listen_addresses
+                .unwrap_or_else(|| Self::LISTEN_ADDRESSES_DEFAULT.to_owned()),
+            pool_size: self.pool_size.unwrap_or(Self::POOL_SIZE_DEFAULT),
+            worker_processes: self.worker_processes.unwrap_or_else(num_cpus::get),
+            connection_string: self.connection_string.unwrap(),
+            table_sources: self.table_sources,
+            function_sources: self.function_sources,
         }
     }
 }
