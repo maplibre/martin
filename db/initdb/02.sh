4c478dd642987349b84c309192b3d0419b67bb40
Birk Skyum <birk.skyum@pm.me>
2022-09-17 16:02:18 +0200

    move to gitlab

--- db/initdb/02.sh
+++ db/initdb/02.sh
@@ -8,26 +8,12 @@ ogr2ogr -f PostgreSQL PG:dbname=db -nln taxi_zones -nlt MULTIPOLYGON -lco GEOMET
 psql -U postgres -d db -c "alter table taxi_zones alter column ${geomColumn} type geometry(multipolygon, 3857) using st_transform(${geomColumn}, 3857); \
   create index idx_taxi_zones_geom on taxi_zones using gist(${geomColumn});"
 
-for i in 01 02 03 04 05 06
+for i in 01
 do
     fileName=yellow_tripdata_2017-${i}.csv
     filePath=${HOME}/${fileName}
 
     wget -O ${filePath} "https://gitlab.com/maplibre1/nyc-data-2017-part-1/-/raw/main/taxo/${fileName}"
-    dos2unix ${filePath}
-    echo "format data"
-    sed -i -e '2d' ${filePath}
-    echo "load into database"
-    psql -U postgres -d db -c "COPY trips FROM '${filePath}' WITH csv header;"
-done
-
-
-for i in 07 08 09 10 11 12
-do
-    fileName=yellow_tripdata_2017-${i}.csv
-    filePath=${HOME}/${fileName}
-
-    wget -O ${filePath} "https://gitlab.com/maplibre1/nyc-data-2017-part-2/-/raw/main/taxo/${fileName}"
     dos2unix ${filePath}
     echo "format data"
     sed -i -e '2d' ${filePath}
