5e1cec6e07b5f69543a36a07515e1b030b0152bf
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2022-10-02 19:24:51 -0400

    cfg cleanup

--- src/config.rs
+++ src/config.rs
@@ -1,23 +1,25 @@
-use crate::{pg, prettify_error, srv};
+use crate::pg::config::{PgConfig, PgConfigBuilder};
+use crate::prettify_error;
+use crate::srv::config::{SrvConfig, SrvConfigBuilder};
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 
-#[derive(Clone, Debug, Serialize)]
+#[derive(Clone, Debug, Serialize, PartialEq, Eq)]
 pub struct Config {
     #[serde(flatten)]
-    pub srv: srv::config::Config,
+    pub srv: SrvConfig,
     #[serde(flatten)]
-    pub pg: pg::config::Config,
+    pub pg: PgConfig,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ConfigBuilder {
     #[serde(flatten)]
-    pub srv: srv::config::ConfigBuilder,
+    pub srv: SrvConfigBuilder,
     #[serde(flatten)]
-    pub pg: pg::config::ConfigBuilder,
+    pub pg: PgConfigBuilder,
 }
 
 /// Update empty option in place with a non-empty value from the second option.
@@ -52,4 +54,114 @@ pub fn read_config(file_name: &str) -> io::Result<ConfigBuilder> {
         .map_err(|e| prettify_error!(e, "Unable to read config file '{}'", file_name))?;
     serde_yaml::from_str(contents.as_str())
         .map_err(|e| prettify_error!(e, "Error parsing config file '{}'", file_name))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::pg::function_source::FunctionSource;
+    use crate::pg::table_source::TableSource;
+    use indoc::indoc;
+    use std::collections::HashMap;
+    use tilejson::Bounds;
+
+    #[test]
+    fn parse_config() {
+        let yaml = indoc! {"
+            ---
+            connection_string: 'postgres://postgres@localhost:5432/db'
+            danger_accept_invalid_certs: false
+            default_srid: 4326
+            keep_alive: 75
+            listen_addresses: '0.0.0.0:3000'
+            pool_size: 20
+            worker_processes: 8
+
+            table_sources:
+              public.table_source:
+                id: public.table_source
+                schema: public
+                table: table_source
+                srid: 4326
+                geometry_column: geom
+                id_column: ~
+                minzoom: 0
+                maxzoom: 30
+                bounds: [-180.0, -90.0, 180.0, 90.0]
+                extent: 4096
+                buffer: 64
+                clip_geom: true
+                geometry_type: GEOMETRY
+                properties:
+                  gid: int4
+
+            function_sources:
+              public.function_source:
+                id: public.function_source
+                schema: public
+                function: function_source
+                minzoom: 0
+                maxzoom: 30
+                bounds: [-180.0, -90.0, 180.0, 90.0]
+        "};
+
+        let config: ConfigBuilder = serde_yaml::from_str(yaml).expect("parse yaml");
+        let config = config.finalize().expect("finalize");
+        let expected = Config {
+            srv: SrvConfig {
+                keep_alive: 75,
+                listen_addresses: "0.0.0.0:3000".to_string(),
+                worker_processes: 8,
+            },
+            pg: PgConfig {
+                connection_string: "postgres://postgres@localhost:5432/db".to_string(),
+                ca_root_file: None,
+                danger_accept_invalid_certs: false,
+                default_srid: Some(4326),
+                pool_size: 20,
+                use_dynamic_sources: false,
+                table_sources: HashMap::from([(
+                    "public.table_source".to_string(),
+                    Box::new(TableSource {
+                        id: "public.table_source".to_string(),
+                        schema: "public".to_string(),
+                        table: "table_source".to_string(),
+                        srid: 4326,
+                        geometry_column: "geom".to_string(),
+                        id_column: None,
+                        minzoom: Some(0),
+                        maxzoom: Some(30),
+                        bounds: Some(Bounds {
+                            left: -180.0,
+                            bottom: -90.0,
+                            right: 180.0,
+                            top: 90.0,
+                        }),
+                        extent: Some(4096),
+                        buffer: Some(64),
+                        clip_geom: Some(true),
+                        geometry_type: Some("GEOMETRY".to_string()),
+                        properties: HashMap::from([("gid".to_string(), "int4".to_string())]),
+                    }),
+                )]),
+                function_sources: HashMap::from([(
+                    "public.function_source".to_string(),
+                    Box::new(FunctionSource {
+                        id: "public.function_source".to_string(),
+                        schema: "public".to_string(),
+                        function: "function_source".to_string(),
+                        minzoom: Some(0),
+                        maxzoom: Some(30),
+                        bounds: Some(Bounds {
+                            left: -180.0,
+                            bottom: -90.0,
+                            right: 180.0,
+                            top: 90.0,
+                        }),
+                    }),
+                )]),
+            },
+        };
+        assert_eq!(config, expected);
+    }
 }
--- src/srv/config.rs
+++ src/srv/config.rs
@@ -7,7 +7,7 @@ pub const LISTEN_ADDRESSES_DEFAULT: &str = "0.0.0.0:3000";
 
 #[derive(clap::Args, Debug)]
 #[command(about, version)]
-pub struct WebServerArgs {
+pub struct SrvArgs {
     #[arg(help = format!("Connection keep alive timeout. [DEFAULT: {}]", KEEP_ALIVE_DEFAULT), short, long)]
     pub keep_alive: Option<usize>,
     #[arg(help = format!("The socket address to bind. [DEFAULT: {}]", LISTEN_ADDRESSES_DEFAULT), short, long)]
@@ -17,22 +17,22 @@ pub struct WebServerArgs {
     pub workers: Option<usize>,
 }
 
-#[derive(Clone, Debug, Serialize)]
-pub struct Config {
+#[derive(Clone, Debug, Serialize, PartialEq, Eq)]
+pub struct SrvConfig {
     pub keep_alive: usize,
     pub listen_addresses: String,
     pub worker_processes: usize,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct ConfigBuilder {
+pub struct SrvConfigBuilder {
     pub keep_alive: Option<usize>,
     pub listen_addresses: Option<String>,
     pub worker_processes: Option<usize>,
 }
 
-impl ConfigBuilder {
-    pub fn merge(&mut self, other: ConfigBuilder) -> &mut Self {
+impl SrvConfigBuilder {
+    pub fn merge(&mut self, other: SrvConfigBuilder) -> &mut Self {
         set_option(&mut self.keep_alive, other.keep_alive);
         set_option(&mut self.listen_addresses, other.listen_addresses);
         set_option(&mut self.worker_processes, other.worker_processes);
@@ -40,8 +40,8 @@ impl ConfigBuilder {
     }
 
     /// Apply defaults to the config, and validate if there is a connection string
-    pub fn finalize(self) -> io::Result<Config> {
-        Ok(Config {
+    pub fn finalize(self) -> io::Result<SrvConfig> {
+        Ok(SrvConfig {
             keep_alive: self.keep_alive.unwrap_or(KEEP_ALIVE_DEFAULT),
             listen_addresses: self
                 .listen_addresses
@@ -51,9 +51,9 @@ impl ConfigBuilder {
     }
 }
 
-impl From<WebServerArgs> for ConfigBuilder {
-    fn from(args: WebServerArgs) -> Self {
-        ConfigBuilder {
+impl From<SrvArgs> for SrvConfigBuilder {
+    fn from(args: SrvArgs) -> Self {
+        SrvConfigBuilder {
             keep_alive: args.keep_alive,
             listen_addresses: args.listen_addresses,
             worker_processes: args.workers,
