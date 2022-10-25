d7bb9c7ad3eb73316cbbb7256b9ce1cefe710c2a
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2022-10-25 00:01:26 -0400

    tmp

--- tests/fixtures/initdb.sh
+++ tests/fixtures/initdb.sh
@@ -4,6 +4,8 @@ set -e
 FIXTURES_DIR="$(dirname "$0")"
 echo "Loading Martin test fixtures into '$PGDATABASE' as user '$PGUSER' from '$FIXTURES_DIR'"
 
+env
+
 psql -v ON_ERROR_STOP=1 -f $FIXTURES_DIR/TileBBox.sql
 psql -v ON_ERROR_STOP=1 -f $FIXTURES_DIR/table_source.sql
 psql -v ON_ERROR_STOP=1 -f $FIXTURES_DIR/table_source_multiple_geom.sql
