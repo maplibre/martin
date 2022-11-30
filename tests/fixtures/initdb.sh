d3a53851713eddbf5fbde715ca7e30385cd02bf0
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2022-11-29 20:32:28 -0500

    move sql files to ensure the order

--- tests/fixtures/initdb.sh
+++ tests/fixtures/initdb.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 FIXTURES_DIR="$(dirname "$0")"
-echo "Loading Martin test fixtures into '$PGDATABASE' as user '$PGUSER' from '$FIXTURES_DIR'"
+echo "Loading Martin test fixtures into '$PGDATABASE' as user '$PGUSER'"
 
 
 psql -P pager=off -v ON_ERROR_STOP=1 -c "CREATE EXTENSION IF NOT EXISTS postgis;"
@@ -11,6 +11,13 @@ psql -P pager=off -v ON_ERROR_STOP=1 -c "DROP SCHEMA IF EXISTS tiger CASCADE;"
 psql -P pager=off -v ON_ERROR_STOP=1 -t -c "select version();"
 psql -P pager=off -v ON_ERROR_STOP=1 -t -c "select PostGIS_Full_Version();"
 
-for sql_file in "$FIXTURES_DIR"/*.sql; do
+
+echo "Importing tables from $FIXTURES_DIR/tables"
+for sql_file in "$FIXTURES_DIR"/tables/*.sql; do
+  psql -e -P pager=off -v ON_ERROR_STOP=1 -f "$sql_file"
+done
+
+echo "Importing functions from $FIXTURES_DIR/functions"
+for sql_file in "$FIXTURES_DIR"/functions/*.sql; do
   psql -e -P pager=off -v ON_ERROR_STOP=1 -f "$sql_file"
 done
