7984acefb7ab3a27246851daf7d757d6442a77ef
Birk Skyum <birk.skyum@pm.me>
2023-05-31 00:52:25 +0200

    Add links to documentation

---
+++ demo/frontend/src/Components/GitHubButton/DocsButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import octocat from './octocat.svg';
+import Container from './Container';
+
+const DocsButton = () => (
+  <Container href='https://maplibre.org/martin' target='_blank'>
+    <span>Documentation</span>
+  </Container>
+);
+
+export default DocsButton;
