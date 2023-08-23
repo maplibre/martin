e1679329c2bb2383cb9f3fff3af1ef88878835a8
rstanciu <rstanciu@rivian.com>
2023-08-23 14:46:44 -0700

    Add tiles_with_hash view to normalized mbtiles files

--- martin-mbtiles/src/tile_copier.rs
+++ martin-mbtiles/src/tile_copier.rs
@@ -271,6 +271,22 @@ impl TileCopier {
             }
         };
 
+        if *dst_mbttype == Normalized {
+            query(
+                "CREATE VIEW tiles_with_hash AS
+                          SELECT
+                              map.zoom_level AS zoom_level,
+                              map.tile_column AS tile_column,
+                              map.tile_row AS tile_row,
+                              images.tile_data AS tile_data,
+                              images.tile_id AS tile_hash
+                          FROM map
+                          JOIN images ON images.tile_id = map.tile_id",
+            )
+            .execute(&mut *conn)
+            .await?;
+        }
+
         query("INSERT INTO metadata SELECT * FROM sourceDb.metadata")
             .execute(&mut *conn)
             .await?;
