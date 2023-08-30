b0650c6ab3ca52aa43bfee447d53b46aac2d1a31
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2023-08-29 21:49:44 -0400

    update homebrew

--- homebrew-formula/martin.rb
+++ homebrew-formula/martin.rb
@@ -1,10 +1,13 @@
 class Martin < Formula
-  current_version="0.8.1"
+  current_version="0.8.7"
 
-  desc "PostGIS Mapbox Vector Tiles server"
-  homepage "https://github.com/urbica/martin"
+  desc "Blazing fast and lightweight tile server with PostGIS, MBTiles, and PMTiles support"
+  homepage "https://github.com/maplibre/martin"
   url "https://github.com/maplibre/martin/releases/download/v#{current_version}/martin-Darwin-x86_64.tar.gz"
-  sha256 "554171bc4761c1852f0d6509b16af00d67f2d2b7d78ad20d9f819beb5d967e8a"
+
+  # This is the sha256 checksum of the martin-Darwin-x86_64.tar.gz file
+  # I am not certain if arch64 should have a different sha256 somewhere
+  sha256 "92f660b1bef3a54dc84e4794a5ba02a8817c25f21ce7000783749bbae9e50de1"
   version "#{current_version}"
 
   depends_on "openssl@3"
