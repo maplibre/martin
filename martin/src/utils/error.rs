2e7a179aaf4b7b329fe5b2f0d9e4f10068d59226
Yuri Astrakhan <yuriastrakhan@gmail.com>
2023-10-21 21:37:14 -0400

    Better error reporting for config and other params (#957)
    This prints an extended error message, explaining to the user what
    params must be removed.
    
    Fixes #938

--- martin/src/utils/error.rs
+++ martin/src/utils/error.rs
@@ -1,3 +1,4 @@
+use std::fmt::Write;
 use std::io;
 use std::path::PathBuf;
 
@@ -7,10 +8,30 @@ use crate::sprites::SpriteError;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
+fn elide_vec(vec: &[String], max_items: usize, max_len: usize) -> String {
+    let mut s = String::new();
+    for (i, v) in vec.iter().enumerate() {
+        if i > max_items {
+            let _ = write!(s, " and {} more", vec.len() - i);
+            break;
+        }
+        if i > 0 {
+            s.push(' ');
+        }
+        if v.len() > max_len {
+            s.push_str(&v[..max_len]);
+            s.push('…');
+        } else {
+            s.push_str(v);
+        }
+    }
+    s
+}
+
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("The --config and the connection parameters cannot be used together")]
-    ConfigAndConnectionsError,
+    #[error("The --config and the connection parameters cannot be used together. Please remove unsupported parameters '{}'", elide_vec(.0, 3, 15))]
+    ConfigAndConnectionsError(Vec<String>),
 
     #[error("Unable to bind to {1}: {0}")]
     BindingError(io::Error, String),
