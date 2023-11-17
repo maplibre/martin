cd375ff1b5b555e0d5ccbde48555d1e3dfb3ca53
Yuri Astrakhan <yuriastrakhan@gmail.com>
2023-11-17 02:40:20 -0500

    Transparent errors, reusable get_composite_tile, minor opt (#1010)

--- martin/src/srv/mod.rs
+++ martin/src/srv/mod.rs
@@ -1,7 +1,5 @@
 mod config;
-mod server;
-
 pub use config::{SrvConfig, KEEP_ALIVE_DEFAULT, LISTEN_ADDRESSES_DEFAULT};
-pub use server::{new_server, router, Catalog, RESERVED_KEYWORDS};
 
-pub use crate::source::CatalogSourceEntry;
+mod server;
+pub use server::{get_composite_tile, new_server, router, Catalog, RESERVED_KEYWORDS};
