b28dae446617e49a745f3c4e0dc57a3b00dea5d6
Yuri Astrakhan <yuriastrakhan@gmail.com>
2024-03-16 22:45:43 -0400

    chore: add empty pg table test (#1259)

---
+++ tests/fixtures/tables/points_empty.sql
@@ -0,0 +1,9 @@
+-- This table is intentionally left empty
+CREATE TABLE points_empty
+(
+    gid  SERIAL PRIMARY KEY,
+    geom GEOMETRY
+);
+
+CREATE INDEX ON points_empty USING GIST (geom);
+CLUSTER points_empty_geom_idx ON points_empty;
