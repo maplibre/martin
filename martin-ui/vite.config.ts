c19591cd9cd837ce872ec25835e05d95411046cd
Paige Williams <paige.williams@aclima.io>
2024-07-11 10:29:03 -0700

    use _/assets as assetsDir

--- martin-ui/vite.config.ts
+++ martin-ui/vite.config.ts
@@ -6,14 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     // assets can also be the name of a tile source
-    // so we use /_/* to avoid conflicts
-    outDir: '_',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: '[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]',
-      },
-    },
+    // so we use /_/assets to avoid conflicts
+    assetsDir: '_/assets'
   },
 });
--- martin/build.rs
+++ martin/build.rs
@@ -4,7 +4,7 @@ fn main() -> std::io::Result<()> {
         static_files::NpmBuild::new("../martin-ui")
             .install()?
             .run("build")?
-            .target("../martin-ui/_")
+            .target("../martin-ui/dist")
             .change_detection()
             .to_resource_dir()
             .build()?;
