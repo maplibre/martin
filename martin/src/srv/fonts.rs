bd5938b5a00519fe8731d038195fe65f87209183
Benjamin Zeiss <ben@zeiss.net>
2024-07-28 14:58:14 +0200

    Improved glyph url handling: accept arbitrary suffixes after {end} #1443

--- martin/src/srv/fonts.rs
+++ martin/src/srv/fonts.rs
@@ -1,4 +1,5 @@
 use std::string::ToString;
+use std::str::FromStr;
 
 use actix_web::error::{ErrorBadRequest, ErrorNotFound};
 use actix_web::web::{Data, Path};
@@ -11,19 +12,41 @@ use crate::srv::server::map_internal_error;
 #[derive(Deserialize, Debug)]
 struct FontRequest {
     fontstack: String,
-    start: u32,
-    end: u32,
+    start: String,
+    end: String,
+}
+
+impl FontRequest {
+    fn parse(&self) -> Result<(u32, u32), &'static str> {
+        let start = u32::from_str(&self.start).map_err(|_| "Invalid start value")?;
+        let end = FontRequest::parse_leading_digits(&self.end)?;
+
+        Ok((start, end))
+    }
+
+    fn parse_leading_digits(input: &str) -> Result<u32, &'static str> {
+        let digits: String = input.chars().take_while(|c| c.is_digit(10)).collect();
+        if digits.is_empty() {
+            Err("No leading digits found")
+        } else {
+            digits.parse::<u32>().map_err(|_| "Failed to parse number")
+        }
+    }
 }
 
 #[route(
-    "/font/{fontstack}/{start}-{end}",
+    "/font/{fontstack}/{start}-{end}*",
     method = "GET",
     wrap = "middleware::Compress::default()"
 )]
 #[allow(clippy::unused_async)]
 async fn get_font(path: Path<FontRequest>, fonts: Data<FontSources>) -> ActixResult<HttpResponse> {
+    let (start, end) = path.parse().map_err(|e| {
+        ErrorBadRequest(e.to_string())
+    })?;
+
     let data = fonts
-        .get_font_range(&path.fontstack, path.start, path.end)
+        .get_font_range(&path.fontstack, start, end)
         .map_err(map_font_error)?;
     Ok(HttpResponse::Ok()
         .content_type("application/x-protobuf")
