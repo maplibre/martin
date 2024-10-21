bf00d85a5c8e1bac00adb5764e80f54716d0b26a
Frank Elsinga <frank@elsinga.de>
2024-10-21 21:55:49 +0200

    fixed the service not being registered

--- martin/src/srv/server.rs
+++ martin/src/srv/server.rs
@@ -114,7 +114,9 @@ pub fn router(cfg: &mut web::ServiceConfig, #[allow(unused_variables)] usr_cfg: 
         .service(get_tile);
 
     #[cfg(feature = "sprites")]
-    cfg.service(crate::srv::sprites::get_sprite_json)
+    cfg.service(crate::srv::sprites::get_sprite_sdf_json)
+        .service(crate::srv::sprites::get_sprite_json)
+        .service(crate::srv::sprites::get_sprite_sdf_png)
         .service(crate::srv::sprites::get_sprite_png);
 
     #[cfg(feature = "fonts")]
