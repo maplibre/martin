cca3775cf539fb9f183113dcca238eaba19dcb72
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2025-01-26 22:56:15 -0500

    Fix martin-ui crate packaging
    The martin-ui dir must be part of the martin package, otherwise it does not get included in the `cargo publish -p martin`.  We could also do it with a symlink from inside the `/martin` dir, but that gets a bit confusing - easier to just keep ui inside martin itself, as it is not used anywhere outside of that crate.

--- martin/build.rs
+++ martin/build.rs
@@ -1,10 +1,10 @@
 fn main() -> std::io::Result<()> {
     #[cfg(feature = "webui")]
     {
-        static_files::NpmBuild::new("../martin-ui")
+        static_files::NpmBuild::new("martin-ui")
             .install()?
             .run("build")?
-            .target("../martin-ui/dist")
+            .target("martin-ui/dist")
             .change_detection()
             .to_resource_dir()
             .build()?;
