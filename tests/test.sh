2ff6a5527ea4df216cd96dcae0bc424bebdd6f1c
sharkAndshark <zhangyijunmetro@hotmail.com>
2025-02-03 00:20:24 +0800

    verify if gdal-bin has been installed

--- tests/test.sh
+++ tests/test.sh
@@ -27,6 +27,15 @@ TEST_TEMP_DIR="$(dirname "$0")/mbtiles_temp_files"
 rm -rf "$TEST_TEMP_DIR"
 mkdir -p "$TEST_TEMP_DIR"
 
+# Verify the tools used in the tests are available
+# todo add more verification for other tools like jq file curl sqlite3...
+if ! command -v ogrmerge.py > /dev/null; then
+  echo "gdal-bin is required for testing"
+  echo "For Ubuntu, you could install it with sudo apt update && sudo apt install gdal-bin -y."
+  echo "see more at https://gdal.org/en/stable/download.html#binaries"
+  exit 1
+fi
+
 function wait_for {
     # Seems the --retry-all-errors option is not available on older curl versions, but maybe in the future we can just use this:
     # timeout -k 20s 20s curl --retry 10 --retry-all-errors --retry-delay 1 -sS "$MARTIN_URL/health"
