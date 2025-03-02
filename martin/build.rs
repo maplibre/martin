f2c7b62e488ca7943a301a02f526ed769ad39c56
Frank Elsinga <frank.elsinga@tum.de>
2025-03-02 02:55:22 +0100

    Revert "migrate to using symlinks instead of copying"
    This reverts commit d4437f79cce7ec7c11ba3c717fcd454026c45d90.

--- martin/build.rs
+++ martin/build.rs
@@ -1,32 +1,33 @@
 #[cfg(feature = "webui")]
 /// copies a directory and its contents to a new location recursively
 fn copy_dir_all(
-    src: &std::path::PathBuf,
-    dst: &std::path::PathBuf,
+    src: impl AsRef<std::path::Path>,
+    dst: impl AsRef<std::path::Path>,
     exclude_dirs: &[std::path::PathBuf],
 ) -> std::io::Result<()> {
-    assert!(!exclude_dirs.contains(src) && !exclude_dirs.contains(dst));
-    assert!(src.is_dir());
-
-    // creating symlinks is cheap => recreate instead of sync
-    if dst.exists() {
-        std::fs::remove_dir_all(dst)?;
-    }
-    std::fs::create_dir_all(dst)?;
+    assert!(!exclude_dirs.contains(&src.as_ref().to_path_buf()));
+    assert!(
+        src.as_ref().is_dir(),
+        "source for the copy operation is not an existing directory"
+    );
+    assert!(
+        !dst.as_ref().exists(),
+        "destination for the copy operation must not exist"
+    );
+    std::fs::create_dir_all(&dst)?;
 
     for entry in std::fs::read_dir(src)? {
         let entry = entry?;
-        if exclude_dirs.contains(&entry.path()) {
-            continue;
-        }
-        let target = dst.join(entry.file_name());
-        #[cfg(unix)]
-        std::os::unix::fs::symlink(entry.path(), target)?;
-        #[cfg(windows)]
-        if entry.file_type()?.is_dir() {
-            std::os::windows::fs::symlink_dir(entry.path(), target)
+        let ty = entry.file_type()?;
+        let src_path = entry.path();
+        let dst_path = dst.as_ref().join(entry.file_name());
+        if ty.is_dir() {
+            if exclude_dirs.contains(&src_path) {
+                continue;
+            }
+            copy_dir_all(src_path, &dst_path, exclude_dirs)?;
         } else {
-            std::os::windows::fs::symlink_file(entry.path(), target)
+            std::fs::copy(src_path, dst_path)?;
         }
     }
     Ok(())
@@ -48,6 +49,9 @@ fn main() -> std::io::Result<()> {
             .parse::<std::path::PathBuf>()
             .unwrap();
         let out_martin_ui_dir = out_dir.join("martin-ui");
+        if out_martin_ui_dir.exists() {
+            std::fs::remove_dir_all(&out_martin_ui_dir)?;
+        }
         copy_dir_all(
             &martin_ui_dir,
             &out_martin_ui_dir,
