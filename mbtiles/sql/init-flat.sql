d0a37e27bc1c64a6c2236cbef282ffb263239d79
sdf-jkl <kosta.tarasov@protonmail.com>
2025-05-31 22:09:21 -0400

    created schema init sql files

---
+++ mbtiles/sql/init-flat.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tiles (
+    zoom_level  INTEGER,
+    tile_column INTEGER,
+    tile_row    INTEGER,
+    tile_data   BLOB);
+
+CREATE UNIQUE INDEX tile_index on tiles (
+    zoom_level, tile_column, tile_row);
---
+++ mbtiles/sql/init-normalized.sql
@@ -0,0 +1,35 @@
+CREATE TABLE map (
+    zoom_level  INTEGER,
+    tile_column INTEGER,
+    tile_row    INTEGER,
+    tile_id     TEXT);
+
+CREATE TABLE images (
+    tile_id   TEXT,
+    tile_data BLOB);
+
+CREATE UNIQUE INDEX map_index ON map (
+    zoom_level, tile_column, tile_row);
+CREATE UNIQUE INDEX images_id ON images (
+    tile_id);
+
+CREATE VIEW tiles AS
+    SELECT
+        map.zoom_level   AS zoom_level,
+        map.tile_column  AS tile_column,
+        map.tile_row     AS tile_row,
+        images.tile_data AS tile_data
+    FROM
+        map JOIN images
+        ON images.tile_id = map.tile_id;
+
+CREATE VIEW tiles_with_hash AS
+    SELECT
+        map.zoom_level   AS zoom_level,
+        map.tile_column  AS tile_column,
+        map.tile_row     AS tile_row,
+        images.tile_data AS tile_data,
+        images.tile_id   AS tile_hash
+    FROM
+        map JOIN images
+        ON map.tile_id = images.tile_id;
