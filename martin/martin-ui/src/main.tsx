bb2b5cab950b4fd1755d68eb38bfdff5b411ec4b
Frank Elsinga <frank@elsinga.de>
2025-07-18 07:48:04 +0200

    fix a merge issue

--- martin/martin-ui/src/main.tsx
+++ martin/martin-ui/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './styles/globals.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
