23181d84234ac9285f820d940961fb0f9a1b1f51
Frank Elsinga <frank@elsinga.de>
2025-07-18 07:56:54 +0200

    don't change where the index.css is located

--- martin/martin-ui/src/main.tsx
+++ martin/martin-ui/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/globals.css';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
