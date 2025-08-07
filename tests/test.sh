3cbd18529157dc00e032f3ef6cc35f19d376c512
sharkAndshar <zhangyijunmetro@hotmail.com>
2025-08-07 14:01:20 +0800

    add test cases

--- tests/test.sh
+++ tests/test.sh
@@ -443,10 +443,9 @@ test_png pmt2_0_0_0   pmt2/0/0/0  # HTTP pmtiles
 
 # Test COG sources (only cog-auto-web, with calculated z/x/y)
 test_jsn cog_auto_web  cog-auto-web
-test_png cog_auto_web_3_4_3 cog-auto-web/3/4/3
-test_png cog_auto_web_5_17_12 cog-auto-web/5/17/12
-test_png cog_auto_web_10_553_402 cog-auto-web/10/553/402
 test_png cog_auto_web_14_8854_6443 cog-auto-web/14/8854/6443
+test_png cog_auto_web_13_4427_3221 cog-auto-web/13/4427/3221
+test_png cog_auto_web_12_2213_1610 cog-auto-web/12/2213/1610
 
 # Test sprites
 test_jsn spr_src1      sprite/src1.json
