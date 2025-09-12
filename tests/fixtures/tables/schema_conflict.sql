b3debf2f40ca83e88fad308e3970f86d9b67f1b6
Frank Elsinga <frank@elsinga.de>
2025-09-13 01:56:20 +0200

    fix another issue caused by the recent refactoring

--- tests/fixtures/tables/schema_conflict.sql
+++ tests/fixtures/tables/schema_conflict.sql
@@ -37,6 +37,6 @@ INSERT INTO schema_b.table_name_existing_two_schemas (b_info, b_geom)
 VALUES ('polygon_1', GEOMFROMEWKT('SRID=4326;POLYGON((30 10, 40 40, 20 40, 10 20, 30 10))'));
 
 CREATE INDEX ON schema_a.table_name_existing_two_schemas USING gist (a_geom);
-CLUSTER table_name_existing_two_schemas_geom_idx ON schema_a.table_name_existing_two_schemas;
+CLUSTER table_name_existing_two_schemas_a_geom_idx ON schema_a.table_name_existing_two_schemas;
 CREATE INDEX ON schema_b.table_name_existing_two_schemas USING gist (b_geom);
-CLUSTER table_name_existing_two_schemas_geom_idx ON schema_b.table_name_existing_two_schemas;
+CLUSTER table_name_existing_two_schemas_b_geom_idx ON schema_b.table_name_existing_two_schemas;
