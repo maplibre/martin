c27ac66c707d6e735124ff9632cc6d6fbc407209
Frank Elsinga <frank@elsinga.de>
2025-09-13 16:48:52 +0200

    non-working

--- tests/fixtures/tables/schema_conflict.sql
+++ tests/fixtures/tables/schema_conflict.sql
@@ -40,3 +40,22 @@ CREATE INDEX ON schema_a.table_name_existing_two_schemas USING gist (a_geom);
 CLUSTER table_name_existing_two_schemas_a_geom_idx ON schema_a.table_name_existing_two_schemas;
 CREATE INDEX ON schema_b.table_name_existing_two_schemas USING gist (b_geom);
 CLUSTER table_name_existing_two_schemas_b_geom_idx ON schema_b.table_name_existing_two_schemas;
+
+-- instead of tables or views, we can also mix them
+CREATE TABLE schema_a.table_and_view_two_schemas (
+    a_id SERIAL PRIMARY KEY,
+    a_info TEXT,
+    a_geom GEOMETRY
+);
+
+INSERT INTO schema_a.table_and_view_two_schemas (a_info, a_geom)
+VALUES ('point_1', '0101000020E6100000EC3A2806EDDA61401C2041E87DDA2740');
+
+CREATE INDEX ON schema_a.table_and_view_two_schemas USING gist (a_geom);
+CLUSTER table_and_view_two_schemas_a_geom_idx ON schema_a.table_and_view_two_schemas;
+
+CREATE VIEW schema_b.table_and_view_two_schemas AS SELECT
+    a_id AS b_id,
+    a_info AS b_info,
+    a_geom
+FROM schema_a.table_and_view_two_schemas;
