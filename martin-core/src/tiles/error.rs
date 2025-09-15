1f4f33af762f52e764eecd3fc2d9f5fb5d1b1c6f
Frank Elsinga <frank.elsinga@tum.de>
2025-09-15 02:08:15 +0200

    add an error type

--- martin-core/src/tiles/error.rs
+++ martin-core/src/tiles/error.rs
@@ -1,5 +1,18 @@
 /// A convenience [`Result`] for tiles coming from `martin-core`.
-pub type MartinCoreResult<T> = Result<T, MartinCoreError>;
+pub type MartinCoreResult<T> = Result<T, BoxedMartinCoreError>;
 
 /// Temporary error type for integration purposes.
-pub type MartinCoreError = Box<dyn std::error::Error>;
+pub type BoxedMartinCoreError = Box<dyn std::error::Error>;
+
+/// Errors that can occur during mbtiles processing operations.
+#[derive(thiserror::Error, Debug)]
+pub enum MartinCoreError {
+    /// Errors that can occur during mbtiles processing operations.
+    #[cfg(feature = "mbtiles")]
+    #[error(transparent)]
+    MbtilesError(#[from] super::mbtiles::MbtilesError),
+
+    /// Errors occurring from other sources, not implemented by `martin-core`.
+    #[error(transparent)]
+    OtherError(#[from] Box<dyn std::error::Error>),
+}
