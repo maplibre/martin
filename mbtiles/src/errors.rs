aff57f261657e65e0b3194af735af57059663d76
Frank Elsinga <frank@elsinga.de>
2025-09-26 18:10:36 +0200

    chore(ci): fix clippy::collapsible-if (#2233)
    This PR fixes `clippy::collapsible-if` and slightly improves the error
    messages in the process

--- mbtiles/src/errors.rs
+++ mbtiles/src/errors.rs
@@ -11,8 +11,17 @@ pub enum MbtError {
     #[error("The source and destination MBTiles files are the same: {0}")]
     SameSourceAndDestination(PathBuf),
 
-    #[error("The diff file and source or destination MBTiles files are the same: {0}")]
-    SameDiffAndSourceOrDestination(PathBuf),
+    #[error("The diff file and source MBTiles files are the same: {0}")]
+    SameDiffAndSource(PathBuf),
+
+    #[error("The diff file and destination MBTiles files are the same: {0}")]
+    SameDiffAndDestination(PathBuf),
+
+    #[error("The patch file and source MBTiles files are the same: {0}")]
+    SamePatchAndSource(PathBuf),
+
+    #[error("The patch file and destination MBTiles files are the same: {0}")]
+    SamePatchAndDestination(PathBuf),
 
     #[error(transparent)]
     SqlxError(#[from] sqlx::Error),
