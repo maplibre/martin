2d86f8c2dd9869666b335235f5ae862989082b33
Frank Elsinga <frank@elsinga.de>
2025-10-06 06:10:51 +0200

    Update martin/src/config/file/file_config.rs

--- martin/src/config/file/file_config.rs
+++ martin/src/config/file/file_config.rs
@@ -495,7 +495,7 @@ fn parse_url(is_enabled: bool, path: &Path) -> Result<Option<Url>, ConfigFileErr
     }
     let url_schemes = [
         "s3://", "s3a://", "gs://", "adl://", "azure://", "abfs://", "abfss://", "http://",
-        "https://",
+        "https://", "file://",
     ];
     path.to_str()
         .filter(|v| url_schemes.iter().any(|scheme| v.starts_with(scheme)))
