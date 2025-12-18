eaeae0328b6fdc3b2e403275fa412485979d8395
CommanderStorm <CommanderStorm@users.noreply.github.com>
2025-12-18 09:48:02 +0000

    deploy: 5e99ad23dee1afb3709094b8716238095e8e18f9

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-82740566.js',
+                path_to_root + 'searchindex-bba8c7ec.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
