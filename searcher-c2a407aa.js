c8a1a3bf0ebb204ae9ba760079adace5523d6dd8
nyurik <nyurik@users.noreply.github.com>
2025-12-20 04:03:48 +0000

    deploy: 47213a62053613ebb82aa75a3eb3b82d3a7083a1

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bba8c7ec.js',
+                path_to_root + 'searchindex-bcd0a95d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
