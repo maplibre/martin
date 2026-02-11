097874b194f0c28e7fac00753a1611305003b298
Frank Elsinga <frank@elsinga.de>
2026-02-11 01:01:30 +0100

    Apply suggestions from code review

--- martin/src/bin/martin.rs
+++ martin/src/bin/martin.rs
@@ -49,11 +49,13 @@ async fn start(args: Args) -> MartinResult<()> {
 
     #[cfg(all(feature = "webui", not(docsrs)))]
     if web_ui_mode == martin::config::args::WebUiMode::EnableForAll {
-        tracing::warn!("Web UI is enabled for all connections at {base_url}/");
+        tracing::info!("Martin server is now active at {base_url}/"");
     } else {
-        info!(
-            "Web UI is disabled. Use `--webui enable-for-all` in CLI or a config value to enable it for all connections."
-        );
+info!(
+"Web UI is disabled. Use `--webui enable-for-all` in CLI or a config value to enable it for all connections."
+);
+info!(
+"Martin server is now active. Visit {base_url}/catalog to see all the sources avaliable");
     }
     tracing::warn!("Martin server is now active at {base_url}/");
     server.await
