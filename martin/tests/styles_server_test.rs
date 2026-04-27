ab858f882e0c767aee6a9319de16404e3e89660a
Auspicus <elijahdisch@gmail.com>
2026-04-27 11:31:16 +1000

    fix: add back feature gate for arguments passed to Catalog::new()

--- martin/tests/styles_server_test.rs
+++ martin/tests/styles_server_test.rs
@@ -15,7 +15,11 @@ macro_rules! create_app {
         let state = mock_sources(mock_cfg($sources)).await.0;
         let app = ::actix_web::App::new()
             .app_data(actix_web::web::Data::new(
-                ::martin::srv::Catalog::new(&state).unwrap(),
+                ::martin::srv::Catalog::new(
+                    #[cfg(any(feature = "sprites", feature = "fonts", feature = "styles"))]
+                    &state,
+                )
+                .unwrap(),
             ))
             .app_data(actix_web::web::Data::new(SrvConfig::default()));
 
