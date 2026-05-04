44fb6b283fe234f17ffbf8a718acdf4c8e9a82da
Frank Elsinga <frank@elsinga.de>
2026-05-04 22:14:23 +0200

    Apply suggestion from @CommanderStorm

--- martin/src/config/primitives/auto_option.rs
+++ martin/src/config/primitives/auto_option.rs
@@ -183,5 +183,13 @@ mod tests {
             let parsed: AutoOption<DummyCfg> = serde_yaml::from_str(&yaml).unwrap();
             assert_eq!(v, parsed);
         }
-    }
+#[rstest]
+ #[case::auto(AutoOption::<DummyCfg>::Auto)]
+ #[case::auto(AutoOption::<DummyCfg>::Disabled)]
+ #[case::auto(AutoOption::Explicit(DummyCfg {foo: Some(true),bar: None,}))]
+fn serde_round_trip(v:AutoOption) {
+let yaml = serde_yaml::to_string(&v).unwrap();
+let parsed: AutoOption<DummyCfg> = serde_yaml::from_str(&yaml).unwrap();
+assert_eq!(v, parsed);
+}
 }
