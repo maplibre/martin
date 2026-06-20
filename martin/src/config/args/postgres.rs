e30ac19ca9da38f415cadbe48be8d907fbff9fd8
manbhav234 <manbhav234@gmail.com>
2026-06-20 17:21:19 +0530

    refactor BoundsCalc

--- martin/src/config/args/postgres.rs
+++ martin/src/config/args/postgres.rs
@@ -1,36 +1,15 @@
-use std::time::Duration;
-
-use clap::ValueEnum;
-use enum_display::EnumDisplay;
-use serde::{Deserialize, Serialize};
 use tracing::{info, warn};
 
 use super::connections::Arguments;
 use super::connections::State::{Ignore, Take};
+use super::bounds::BoundsCalcType;
 use crate::config::file::UnrecognizedValues;
 use crate::config::file::postgres::{
     DEFAULT_POOL_SIZE, DEFAULT_RELOAD_INTERVAL, PostgresConfig, PostgresSslCerts,
 };
 use crate::config::primitives::env::Env;
 use crate::config::primitives::{OptBoolObj, OptOneMany};
-// Must match the help string for BoundsType::Quick
-pub const DEFAULT_BOUNDS_TIMEOUT: Duration = Duration::from_secs(5);
 
-#[derive(
-    PartialEq, Eq, Default, Debug, Clone, Copy, Serialize, Deserialize, ValueEnum, EnumDisplay,
-)]
-#[cfg_attr(feature = "unstable-schemas", derive(schemars::JsonSchema))]
-#[serde(rename_all = "lowercase")]
-#[enum_display(case = "Kebab")]
-pub enum BoundsCalcType {
-    /// Compute table geometry bounds, but abort if it takes longer than 5 seconds.
-    #[default]
-    Quick,
-    /// Compute table geometry bounds. The startup time may be significant. Make sure all GEO columns have indexes.
-    Calc,
-    /// Skip bounds calculation. The bounds will be set to the whole world.
-    Skip,
-}
 
 #[derive(clap::Args, Debug, PartialEq, Default)]
 #[command(about, version)]
