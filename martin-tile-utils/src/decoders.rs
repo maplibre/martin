89eb416f005f7fa0191f244b6c10b61b4ad3195c
Paweł Urbanek <contact@pawelurbanek.com>
2026-07-26 23:43:37 +0200

    chore(deps): Update hotpath and instrument decoders (#3059)
    Hi, I just published a new https://hotpath.rs/ release with IO
    instrumentation.
    
    It allows wrapping any IO stream, to collect metrics like bytes
    read/write, rate, perf percentiles etc. with minimal overhead.
    
    I tested with:
    
    ```
    oha -n 100000 -H 'Accept-Encoding: br'       http://localhost:3000/world_cities/0/0/0
    ```
    
    and noticed that brotli compression is significantly slower.
    
    Before:
    
    <img width="485" height="256" alt="before-oha"
    src="https://github.com/user-attachments/assets/8bffdcd4-d983-4ea2-ba9c-28e2b2c03d03"
    />
    <img width="1140" height="194" alt="before-rate"
    src="https://github.com/user-attachments/assets/ecc1c693-3e82-4827-a239-c718604211d0"
    />
    
    After switching to compression level 4: 
    
    <img width="488" height="252" alt="after-oha"
    src="https://github.com/user-attachments/assets/3bb8ec6b-242b-4963-8e98-64e979851f5d"
    />
    <img width="1143" height="203" alt="after-rate"
    src="https://github.com/user-attachments/assets/f159cc61-a91c-4658-a4e3-e0950c89fe6b"
    />
    
    ~30x encoding and response time speedup. Brotli codec rate: `27.9kb/s ->
    1.6 MB/s`
    
    Not sure if this change makes sense for your project i.e. response speed
    vs compression ratio tradeoff. But maybe you'll find the new I/O
    instrumentation useful in other places. It should work for TCP, TLS,
    files I/O basically any types using `Read`, `Write`, `AsyncRead`,
    `AsyncWrite` traits.
    
    I had to reorganize encoders usage, so that all data is captured, but
    final result should be identical.
    
    As always feedback welcome!

--- martin-tile-utils/src/decoders.rs
+++ martin-tile-utils/src/decoders.rs
@@ -1,56 +1,74 @@
-use std::io::{Read as _, Write as _};
+use std::io::Read as _;
 
-use flate2::read::{MultiGzDecoder, ZlibDecoder};
-use flate2::write::GzEncoder;
+use flate2::read::{GzEncoder, MultiGzDecoder, ZlibDecoder, ZlibEncoder};
 
 pub fn decode_gzip(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-    let mut decoder = MultiGzDecoder::new(data);
+    let mut decoder = hotpath::io!(MultiGzDecoder::new(data), label = "decode_gzip");
     let mut decompressed = Vec::new();
     decoder.read_to_end(&mut decompressed)?;
     Ok(decompressed)
 }
 
 pub fn encode_zlib(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-    let mut encoder = flate2::write::ZlibEncoder::new(Vec::new(), flate2::Compression::default());
-    encoder.write_all(data)?;
-    encoder.finish()
+    let mut encoder = hotpath::io!(
+        ZlibEncoder::new(data, flate2::Compression::default()),
+        label = "encode_zlib"
+    );
+    let mut compressed = Vec::new();
+    encoder.read_to_end(&mut compressed)?;
+    Ok(compressed)
 }
 
 pub fn decode_zlib(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-    let mut decoder = ZlibDecoder::new(data);
+    let mut decoder = hotpath::io!(ZlibDecoder::new(data), label = "decode_zlib");
     let mut decompressed = Vec::new();
     decoder.read_to_end(&mut decompressed)?;
     Ok(decompressed)
 }
 
 pub fn encode_gzip(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-    let mut encoder = GzEncoder::new(Vec::new(), flate2::Compression::default());
-    encoder.write_all(data)?;
-    encoder.finish()
+    let mut encoder = hotpath::io!(
+        GzEncoder::new(data, flate2::Compression::default()),
+        label = "encode_gzip"
+    );
+    let mut compressed = Vec::new();
+    encoder.read_to_end(&mut compressed)?;
+    Ok(compressed)
 }
 
 pub fn decode_brotli(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-    let mut decoder = brotli::Decompressor::new(data, 4096);
+    let mut decoder = hotpath::io!(
+        brotli::Decompressor::new(data, 4096),
+        label = "decode_brotli"
+    );
     let mut decompressed = Vec::new();
     decoder.read_to_end(&mut decompressed)?;
     Ok(decompressed)
 }
 
 pub fn encode_brotli(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-    let mut encoder = brotli::CompressorWriter::new(Vec::new(), 4096, 11, 22);
-    encoder.write_all(data)?;
-    Ok(encoder.into_inner())
+    let mut encoder = hotpath::io!(
+        brotli::CompressorReader::new(data, 4096, 11, 22),
+        label = "encode_brotli"
+    );
+    let mut compressed = Vec::new();
+    encoder.read_to_end(&mut compressed)?;
+    Ok(compressed)
 }
 
 pub fn decode_zstd(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-    let mut decoder = zstd::Decoder::new(data)?;
+    let mut decoder = hotpath::io!(zstd::Decoder::new(data)?, label = "decode_zstd");
     let mut decompressed = Vec::new();
     decoder.read_to_end(&mut decompressed)?;
     Ok(decompressed)
 }
 
 pub fn encode_zstd(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-    let mut encoder = zstd::Encoder::new(Vec::new(), 0)?;
-    encoder.write_all(data)?;
-    encoder.finish()
+    let mut encoder = hotpath::io!(
+        zstd::stream::read::Encoder::new(data, 0)?,
+        label = "encode_zstd"
+    );
+    let mut compressed = Vec::new();
+    encoder.read_to_end(&mut compressed)?;
+    Ok(compressed)
 }
