03eb2d50aab3d932c12ae27826bb0e29ea83840b
Frank Elsinga <frank@elsinga.de>
2026-08-17 23:59:41 +0200

    Apply suggestion from @CommanderStorm

--- martin/src/config/file/srv.rs
+++ martin/src/config/file/srv.rs
@@ -33,7 +33,7 @@ impl CacheControlHeader {
 
 impl fmt::Display for CacheControlHeader {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        #[expect(clippy::map-err-ignore, reason = "cannot be returned and is not usefull to log")]
+        #[expect(clippy::map_err_ignore, reason = "cannot be returned and is not usefull to log")]
         f.write_str(self.0.to_str().map_err(|_| fmt::Error)?)
     }
 }
